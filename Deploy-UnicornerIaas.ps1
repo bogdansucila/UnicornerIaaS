796047cdf83c36af3544f0cee14cf0625b803117
Bogdan Sucila <bogdan.sucila@endava.com>
2017-06-07 12:58:18 +0300

    Initial commit

---
+++ Deploy-UnicornerIaas.ps1
@@ -0,0 +1,33 @@
+﻿### Define variables
+{
+$location = 'West Europe'
+$resourceGroupName = 'unicorner-iaas'
+$resourceDeploymentName = 'unicorner-iaas-deployment'
+$templatePath = $env:SystemDrive + '\Pluralsight'
+$templateFile = 'unicornerIaas.json'
+$template = $templatePath + '\' + $templateFile
+$password = "Un1c0rn3R"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['vmPrivateAdminPassword'] = $securePassword
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    @additionalParameters `
+    -Verbose -Force
+}
